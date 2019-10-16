26ee3a135508e5d2d752bfba93abba1a06f05a45
Sebastian Andréasson <sebastian.andreasson93@gmail.com>
2019-10-16 15:59:12 +0200

    add bankID login

---
+++ data.js
@@ -0,0 +1,75 @@
+const companies = [
+  {
+    id: 1,
+    name: "Iteam",
+    token: 'a-token',
+    main: true,
+    active: true,
+  },
+  {
+    id: 2,
+    name: "PE Accounting",
+    token: 'another-token2',
+    main: false,
+    active: true,
+  },
+  {
+    id: 3,
+    name: "Sebastians Sajter",
+    token: 'another-token3',
+    main: false,
+    active: true,
+  },
+  {
+    id: 4,
+    name: "Andreas Ananasjuice",
+    token: 'another-token4',
+    main: false,
+    active: true,
+  },
+  {
+    id: 5,
+    name: "Jonnas Jiu-jitsuklubb",
+    token: 'another-token5',
+    main: false,
+    active: true,
+  },
+  {
+    id: 6,
+    name: "Viktors VVS",
+    token: 'another-token6',
+    main: false,
+    active: true,
+  },
+  {
+    id: 7,
+    name: "Toms Tombolas",
+    token: 'another-token7',
+    main: false,
+    active: true,
+  },
+  {
+    id: 8,
+    name: "Emils Emaljögon",
+    token: 'another-token8',
+    main: false,
+    active: true,
+  },
+  {
+    id: 9,
+    name: "Albins Arbetarfik",
+    token: 'another-token9',
+    main: false,
+    active: true,
+  },
+  {
+    id: 10,
+    name: "Jennys Jeansfärgade Jackor",
+    token: 'another-token10',
+    main: false,
+    active: true,
+  }
+]
+module.exports = {
+  companies
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = require('express')()
 const mobileApp = require('express')()
 const moment = require('moment')
 const expenseFileBinaryHex = require('./expenseFileBinaryHex.js')
+const { companies } = require('./Data')
 
 const wait = (time) => new Promise(resolve => setTimeout(() => resolve(), time))
 
@@ -49,78 +50,7 @@ mobileApp.post('/mobile-api/ws/user/login', (req, res) => {
     })
   }
   console.log('login', req.body)
-  res.send([
-    {
-      id: 1,
-      name: "Iteam",
-      token: 'a-token',
-      main: true,
-      active: true,
-    },
-    {
-      id: 2,
-      name: "PE Accounting",
-      token: 'another-token2',
-      main: false,
-      active: true,
-    },
-    {
-      id: 3,
-      name: "Sebastians Sajter",
-      token: 'another-token3',
-      main: false,
-      active: true,
-    },
-    {
-      id: 4,
-      name: "Andreas Ananasjuice",
-      token: 'another-token4',
-      main: false,
-      active: true,
-    },
-    {
-      id: 5,
-      name: "Jonnas Jiu-jitsuklubb",
-      token: 'another-token5',
-      main: false,
-      active: true,
-    },
-    {
-      id: 6,
-      name: "Viktors VVS",
-      token: 'another-token6',
-      main: false,
-      active: true,
-    },
-    {
-      id: 7,
-      name: "Toms Tombolas",
-      token: 'another-token7',
-      main: false,
-      active: true,
-    },
-    {
-      id: 8,
-      name: "Emils Emaljögon",
-      token: 'another-token8',
-      main: false,
-      active: true,
-    },
-    {
-      id: 9,
-      name: "Albins Arbetarfik",
-      token: 'another-token9',
-      main: false,
-      active: true,
-    },
-    {
-      id: 10,
-      name: "Jennys Jeansfärgade Jackor",
-      token: 'another-token10',
-      main: false,
-      active: true,
-    }
-  ])
+  res.send(companies)
 })
 
 const createExpense = (i) => ({
@@ -220,4 +150,23 @@ mobileApp.get('/mobile-api/ws/expense/unprocessedExpenseFiles', async (req, res)
     { registrationDate: new Date().toISOString(), amount: 111, thumbnail: expenseFileBinaryHex.peLogo},
     { registrationDate: new Date().toISOString(), amount: 222, thumbnail: expenseFileBinaryHex.peLogo}
   ])
+})
+
+let bankIdProgressCompleted
+mobileApp.post('/mobile-api/ws/user/bankid/authenticate', (req, res) => {
+  res.send({
+    orderRef: 'some-order-ref',
+    autoStartToken: 'some-auto-start-token'
+  })
+  bankIdProgressCompleted = false
+  setTimeout(() => {
+    bankIdProgressCompleted = true
+  }, 3000)
+})
+
+mobileApp.post('/mobile-api/ws/user/bankid/progress', (req, res) => {
+  res.send({
+    status: bankIdProgressCompleted ? 'complete': 'inprogress',
+    companies
+  })
 })
